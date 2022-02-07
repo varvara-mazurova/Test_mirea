aedfc197382e8a15c18cf9f2777e8a17043eafce
varvara <varia.mazurovaa@mail.ru>
2022-02-07 13:04:47 +0300

    second task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,31 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String string = sc.nextLine();
-        int summa = 0;
-        for (int i = 0; i < string.length(); i++){
-            char char_of_string = string.charAt(i);
-            if(Character.isDigit(char_of_string) == true){
-                summa+=Character.getNumericValue(char_of_string);
+        double number = sc.nextDouble();
+        if(number != (int)number){
+            System.out.println("Ошибка, вы ввели нецелое число!");
+        }
+        else{
+            for(int i = 2; i <= number; i++){
+                int flag = 0;
+                if((int)number % i == 0){
+                    for(int j = 2; j < i; j++){
+                        if(i % j == 0){
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if(flag == 0){
+                        System.out.print(i + " ");
+                    }
+                }
             }
         }
-        System.out.println(summa);
     }
 }
